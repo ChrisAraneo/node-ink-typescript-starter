a8d47e9fdab33b4db4278e4cfe162a3ab2ee1d88
Krzysztof Pająk <chris.araneo@gmail.com>
2024-11-05 21:19:16 +0100

    chore: update deps, remove xo, add tsimp, add & use eslint plugins, update scripts, update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginJsonc from 'eslint-plugin-jsonc';
+import eslintPluginReact from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  (eslintPluginReact.configs.flat || {}).recommended,
+  (eslintPluginReact.configs.flat || {})['jsx-runtime'],
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'jsonc/no-comments': 'error',
+      'jsonc/sort-keys': 'error',
+    },
+    ignores: ['**/node_modules/', '.git/', 'package.json', 'package-lock.json'],
+  },
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react: eslintPluginReact,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
